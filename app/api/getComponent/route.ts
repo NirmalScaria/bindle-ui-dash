b928f4daa85ba6abb4502613af663ec872360353
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-08-03 21:04:15 +0530

    [FEATURE] Button code

--- app/api/getComponent/route.ts
+++ app/api/getComponent/route.ts
@@ -7,12 +7,38 @@ type ResponseData = {
     message: string
 }
 
+type ComponentData = {
+    name: string
+    location: string
+    dependencies: string[]
+    content: string
+}
+
+const knownComponents: { [id: string]: ComponentData } = {
+    "button": {
+        name: 'Button',
+        location: '.',
+        dependencies: [
+
+        ],
+        content: `
+      import React from 'react';
+      export function Button({ children }: { children: string }) {
+        return <button>{children}</button>;
+      }
+      `
+    }
+}
+
 export async function GET(req: NextRequest, res: NextApiResponse<ResponseData>) {
     const params = req.nextUrl.searchParams
-    const componentId = params.get('componentId')
-    const useTypescript = params.get('useTypescript') === 'true'
+    const componentId: string | null = params.get('componentId')
+    const useTypescript: boolean = params.get('useTypescript') === 'true'
     if (!componentId) {
         return NextResponse.json({ message: "Component ID is required", success: false })
     }
-    return NextResponse.json({ message: `Component ID: ${componentId}, useTypescript: ${useTypescript}`, success: true })
+    if (componentId in knownComponents === false) {
+        return NextResponse.json({ message: "Component ID is not recognized", success: false })
+    }
+    return NextResponse.json({ message: "Component found", success: true, component: JSON.stringify(knownComponents[componentId]) })
 }
