3151f71a26ae1fcf1904171b96cc4fa59868dc12
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-08-08 17:17:46 +0530

    [FIX] Footer responsive

--- components/docsFooter.tsx
+++ components/docsFooter.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function DocsFooter() {
     return <div className="flex flex-col md:flex-row p-3 border-t mt-3 w-full text-sm text-gray-500">
-        Created by&nbsp; <Link href="https://www.linkedin.com/in/nirmal-scaria/" className="underline">Nirmal Scaria</Link>.
-        Source code on &nbsp;<Link href="https://github.com/NirmalScaria/bindle-ui" className="underline">GitHub</Link>
+        <div>Created by&nbsp; <Link href="https://www.linkedin.com/in/nirmal-scaria/" className="underline">Nirmal Scaria</Link>.</div>
+        <div>Source code on &nbsp;<Link href="https://github.com/NirmalScaria/bindle-ui" className="underline">GitHub</Link></div>
     </div>
 }
