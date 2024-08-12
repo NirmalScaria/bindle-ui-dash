b04c15900f33eb7860f06c15d5bd1884900e8f1f
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2024-08-12 09:54:21 +0530

    [FIX][UI] Dark to light

--- components/customSandpackEditor.tsx
+++ components/customSandpackEditor.tsx
@@ -19,8 +19,8 @@ export default function CustomSandpackEditor({height, ...props }: {height: strin
     const [showCode, setShowCode] = useState(false);
     return <div className="flex flex-col w-full">
         <div className="flex flex-row mb-5 border-b">
-            <button onClick={() => setShowCode(false)} className={cn("p-2 text-sm", showCode ? "" : "border-b-2 border-b-white")}>Preview</button>
-            <button onClick={() => setShowCode(true)} className={cn("p-2 text-sm", !showCode ? "" : "border-b-2 border-b-white")}>Code</button>
+            <button onClick={() => setShowCode(false)} className={cn("p-2 text-sm", showCode ? "" : "border-b-2 border-b-primary")}>Preview</button>
+            <button onClick={() => setShowCode(true)} className={cn("p-2 text-sm", !showCode ? "" : "border-b-2 border-b-primary")}>Code</button>
         </div>
         <SandpackProvider template="react-ts" theme={githubLight} {...props}>
             <SandpackLayout style={{ visibility: !showCode ? "visible" : "hidden", height: !showCode ? "auto" : "1px" }} >
